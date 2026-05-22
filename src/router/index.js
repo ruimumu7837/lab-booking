8b04f27162984fbc3f929cc7f088e4b4e251fc07
ruimumu7837 <3033623326@qq.com>
2026-05-22 12:22:15 +0800

    feat: 前端预约、统计页面 + 注册功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Equipment from '../views/Equipment.vue'
+import Booking from '../views/Booking.vue'
+import Statistics from '../views/Statistics.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
   { path: '/register', component: () => import('../views/Register.vue') },
   { path: '/equipment', component: Equipment, meta: { auth: true } },
-  // 后面再加 booking 和 statistics
+  { path: '/booking', component: Booking, meta: { auth: true } },
+  { path: '/statistics', component: Statistics, meta: { auth: true } }
 ]
+
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !localStorage.getItem('token')) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
